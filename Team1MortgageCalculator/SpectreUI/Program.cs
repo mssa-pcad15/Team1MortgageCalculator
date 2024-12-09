be78fc3f8acb8e0ff559a8b2d8a8fe1adb02c91f
Okino Kamali Leiba <okino.leiba@yahoo.com>
2024-12-09 21:58:50 +0000

    update code with layout

--- Team1MortgageCalculator/SpectreUI/Program.cs
+++ Team1MortgageCalculator/SpectreUI/Program.cs
@@ -1,11 +1,31 @@
 ﻿using Spectre.Console;
+using Spectre.Console.Cli;
 namespace SpectreUI
+
 {
 	internal class Program
 	{
 		static void Main(string[] args)
 		{
-			AnsiConsole.Markup("[underline red]Hello[/] World!");
+			// Create the layout
+			var layout = new Layout("Root")
+				.SplitRows(
+					new Layout("Top").SplitColumns(
+						new Layout("Left"),
+						new Layout("Right")),
+					new Layout("Bottom"));
+						
+
+			//// Update the left column
+			//layout["Left"].Update(
+			//	new Panel(
+			//		Align.Center(
+			//			new Markup("Hello [blue]World![/]"),
+			//			VerticalAlignment.Middle))
+			//		.Expand());
+
+			// render the layout
+			AnsiConsole.Write(layout);
 		}
 	}
 }
