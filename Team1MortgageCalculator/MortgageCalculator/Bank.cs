63afe29788e3913935a517dfc41680552740eafb
Okino Kamali Leiba <okino.leiba@yahoo.com>
2024-12-10 21:25:51 +0000

    add more bank methods to calulate total: loan amount, prinicpal payment, etc. and late charges

--- Team1MortgageCalculator/MortgageCalculator/Bank.cs
+++ Team1MortgageCalculator/MortgageCalculator/Bank.cs
@@ -1,24 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 
 namespace MortgageCalculator
 {
 	internal class Bank
 	{
+		public const decimal latePaymentRate =  5m;
+
+
 		MortgageCalc mortgageCalc = new MortgageCalc(10000m, 5m, 30);
 		//LoanInformation loanInformation = new LoanInformation(10000m, 5m, 30);
 
 
-		//public decimal CalculateTotalInterest()
-		//{
-		//	return mortgageCalc.LoanInformation.LoanAmount * Math.Pow((double)(1 + mortgageCalc.LoanInformation.InterestRate), (mortgageCalc.LoanInformation.LoanDuration / 12) - mortgageCalc.LoanInformation.LoanAmount;
+		public decimal CalculateTotalInterest() => mortgageCalc.LoanInformation.LoanAmount * (decimal)Math.Pow((double)(1 + mortgageCalc.LoanInformation.InterestRate), decimal.ToDouble(mortgageCalc.LoanInformation.LoanDuration / 12m)) - mortgageCalc.LoanInformation.LoanAmount;
 
-		//	MortgageCalc mortgageCalc = new MortgageCalc();
-		//}
+		public decimal CalculateTotalPrincipalPayment() => mortgageCalc.PrincipalPayment(1) * mortgageCalc.LoanInformation.LoanDuration;
 
+		public decimal CalculateTotalCostLoan() => mortgageCalc.TotalCost;
+
+		public decimal CalculationLatePaymentCharges(bool lateIndicator, int month)
+		{
+
+			if (lateIndicator && DateTime.Now.Day < 10)
+			{
+				return mortgageCalc.PrincipalPayment(month) * latePaymentRate + mortgageCalc.PrincipalPayment(month);
+			}
+			else {
+				return mortgageCalc.PrincipalPayment(month) * latePaymentRate + mortgageCalc.PrincipalPayment(month) + 50m;
+
+			}
+		}
 	}
 }
